f8face71a89b2ad17d64785c9a35615e7c5b3a95
wjg <huozhanbai@baofu.com>
2017-01-03 13:42:11 +0800

    quartz定时任务

---
+++ admin-web/src/main/java/org/jigang/plat/admin/quartz/test/TestTaskService.java
@@ -0,0 +1,16 @@
+package org.jigang.plat.admin.quartz.test;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * 在此处添加类注释
+ *
+ * @author wjg
+ * @date 2017/1/3.
+ */
+@Service
+public class TestTaskService {
+    public void test() {
+        System.out.println("\n>>>>>>>>>>>>>>>>>>>>>>测试任务执行成功");
+    }
+}
